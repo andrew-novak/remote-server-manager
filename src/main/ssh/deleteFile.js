d2fd4d5b201488065e9f26bd34425ab91af5dae9
Andrew Novak <andrew.novak.developer@gmail.com>
2021-05-07 19:35:58 +0100

    possible to delete a directory

--- src/main/ssh/deleteFile.js
+++ src/main/ssh/deleteFile.js
@@ -1,15 +1,26 @@
-import Client from "ssh2-sftp-client";
+import { Client } from "ssh2";
 
+import ssh2DataToArray from "./ssh2DataToArray";
 import prepOptions from "./prepOptions";
 
-export default async (sshOptions, path) => {
-  if (!sshOptions || !path) return { error: "Pass all required arguments" };
-  const sftp = new Client();
-  try {
-    await sftp.connect(prepOptions(sshOptions));
-    await sftp.delete(path);
-  } catch (err) {
-    return err.message;
-  }
-  return null;
-};
+export default (sshOptions, path) =>
+  new Promise((resolve) => {
+    if (!sshOptions || !path) resolve({ error: "Pass all required arguments" });
+    const ssh2 = new Client();
+    ssh2
+      .on("ready", () => {
+        ssh2.exec(`rm -r ${path}`, (err, stream) => {
+          if (err) resolve({ error: err.message });
+          stream.on("exit", () => {
+            ssh2.end();
+            resolve();
+          });
+          stream.stderr.on("data", (data) => {
+            const arr = ssh2DataToArray(data, ": ");
+            const error = arr[2];
+            resolve({ error });
+          });
+        });
+      })
+      .connect(prepOptions(sshOptions));
+  });
